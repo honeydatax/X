e8508f798d555737394b126cac5e2204516da605
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-10-03 15:04:38 +0100

    source code

---
+++ decompiler.cpp
@@ -0,0 +1,36 @@
+#include "decompiler.h"
+int main(int argc, char *argv[])
+{
+	
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "decompiler.glade", NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "form1"));
+    window2 = GTK_WIDGET(gtk_builder_get_object(builder, "form2"));
+    button1 = GTK_WIDGET(gtk_builder_get_object(builder, "button3"));
+    text3 = GTK_WIDGET(gtk_builder_get_object(builder, "text2"));
+  
+	g_signal_connect_swapped (window , "destroy",
+			      G_CALLBACK (on_destroy),
+			    window );
+			     
+	g_signal_connect_swapped (window , "selection-changed",
+			      G_CALLBACK (on_select),
+			    window ); 
+
+	g_signal_connect_swapped (button1 , "pressed",
+			      G_CALLBACK (on_click),
+			    window ); 
+
+	g_signal_connect_swapped (window2 , "response",
+			      G_CALLBACK (on_acivate),
+			    window );
+	
+   
+    g_object_unref(builder);
+    gtk_widget_show(window);                
+    gtk_main();
+    return 0;
+}
